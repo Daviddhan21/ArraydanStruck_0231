f5ae1236f189e1275e69b6fed203714ceab9f0fd
Daviddhan21 <rayvanza.davida.ft24@mail.umy.ac.id>
2026-04-06 18:43:42 +0700

    Mengisi Data

---
+++ struck.cpp
@@ -0,0 +1,9 @@
+
+
+    // menampilkan data
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+ 
+}
